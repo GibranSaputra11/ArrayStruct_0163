71393d9475d5401325e0a025974325d53a2ef665
GibranSaputra11 <gibranbantul@gmail.com>
2025-04-14 15:35:06 +0700

    add array kosong yang akan diisi di ahir

--- Array.cpp
+++ Array.cpp
@@ -7,6 +7,9 @@ string matkul[] = {"Pemrograman", "Algoritma"};
 // sebuah arry yang memiliki ukuran
 float nilaiAkhir[2] = {1, 2.5};
 
+//membuat array yang masih kosong
+char grade[5];
+
 int main()
 {
 
