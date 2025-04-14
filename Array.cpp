b321c50d03817b5faca0f3a4cef95452ec8a00f1
GibranSaputra11 <gibranbantul@gmail.com>
2025-04-14 15:34:28 +0700

    add array memiliki ukuran

--- Array.cpp
+++ Array.cpp
@@ -4,7 +4,10 @@ using namespace std;
 // sebuah arry yang tidak memiliki ukuran
 string matkul[] = {"Pemrograman", "Algoritma"};
 
+// sebuah arry yang memiliki ukuran
+float nilaiAkhir[2] = {1, 2.5};
+
 int main()
 {
-    
+
 }
